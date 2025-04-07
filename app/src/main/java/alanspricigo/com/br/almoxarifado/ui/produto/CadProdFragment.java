cdfa4f3c6b1c533a8f07f01eed0ea83fb12e0c42
yDrug <alanspricigo123@gmail.com>
2025-04-07 10:51:54 -0300

    0.5

--- app/src/main/java/alanspricigo/com/br/almoxarifado/ui/produto/CadProdFragment.java
+++ app/src/main/java/alanspricigo/com/br/almoxarifado/ui/produto/CadProdFragment.java
@@ -1,5 +1,6 @@
 package alanspricigo.com.br.almoxarifado.ui.produto;
 
+import android.content.Context;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -12,7 +13,19 @@ import android.widget.CalendarView;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+import com.google.android.material.snackbar.Snackbar;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.Date;
 
 import alanspricigo.com.br.almoxarifado.R;
@@ -23,7 +36,7 @@ import alanspricigo.com.br.almoxarifado.model.Dados;
  * Use the {@link CadProdFragment#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class CadProdFragment extends Fragment implements View.OnClickListener {
+public class CadProdFragment extends Fragment implements View.OnClickListener, Response.ErrorListener, Response.Listener {
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -41,6 +54,10 @@ public class CadProdFragment extends Fragment implements View.OnClickListener {
     private CalendarView cvDataEntrada;
     private EditText etFabr;
     private Button buttonCad;
+
+    //volley
+    private RequestQueue requestQueue;
+    private JsonObjectRequest jsonObjectReq;
 
     public CadProdFragment() {
         // Required empty public constructor
@@ -84,12 +101,17 @@ public class CadProdFragment extends Fragment implements View.OnClickListener {
         this.cvDataEntrada = view.findViewById(R.id.cvDataEntrada);
         this.etFabr = view.findViewById(R.id.etFabr);
         this.buttonCad = view.findViewById(R.id.buttonCad);
-
         this.buttonCad.setOnClickListener(this);
+
+
+
+        //instanciando a fila de requests - caso o objeto seja o view
+        this.requestQueue = Volley.newRequestQueue(view.getContext());
+//inicializando a fila de requests do SO
+        this.requestQueue.start();
 
         return view;
     }
-
     @Override
     public void onClick(View v) {
         if (view.getId() == R.id.buttonCad)
@@ -104,10 +126,50 @@ public class CadProdFragment extends Fragment implements View.OnClickListener {
                 SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
                 String dataSelecionada = sdf.format(new Date(cvDataEntrada.getDate()));
                 dados.setFabrProd(this.etFabr.getText().toString());
+                jsonObjectReq = new JsonObjectRequest(
+                        Request.Method.POST,
+                        "http://10.0.2.2:8080/seg/cadusuario.php",
+                        dados.toJsonObject(), this, this);
+                requestQueue.add(jsonObjectReq);
                 Toast.makeText(view.getContext(), "Cadastro com sucesso.", Toast.LENGTH_SHORT).show();
             } catch (Exception e) {
                 throw new RuntimeException(e);
             }
         }
     }
-}
+    @Override
+    public void onErrorResponse(VolleyError error)
+    {
+        Snackbar mensagem = Snackbar.make(view,
+                "Ops! Houve um problema ao realizar o cadastro: " +
+                        error.toString(),Snackbar.LENGTH_LONG);
+        mensagem.show();
+
+    }
+    @Override
+    public void onResponse(Object response) {
+        try {
+            //instanciando objeto para manejar o JSON que recebemos
+            JSONObject json = new JSONObject(response.toString());
+            Context context = view.getContext();
+            //pegando mensagem que veio do json
+            CharSequence mensagem = json.getString("message");
+            //duração da mensagem na tela
+            int duration = Toast.LENGTH_SHORT;
+            //verificando se salvou sem erro para limpar campos da tela
+            if (json.getBoolean("success")){
+//limpar campos da tela
+                this.etName.setText("");
+                this.etCod.setText("");
+                this.etFabr.setText("");
+                Calendar calendar = Calendar.getInstance();
+                long today = calendar.getTimeInMillis();
+                this.cvDataEntrada.setDate(today, false, true);
+            }
+//mostrando a mensagem que veio do JSON
+            Toast toast = Toast.makeText (context, mensagem, duration);
+            toast.show();
+        } catch (JSONException e) {
+            throw new RuntimeException(e);
+        }
+    }
